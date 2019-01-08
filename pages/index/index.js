0de087e6f2113b6f3928845ace3e87bb3a1f86f6
arthen <641700486@qq.com>
2019-01-09 00:08:18 +0800

    地区时间选择器完善

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,29 +2,29 @@
 var app = getApp();
 
 var cityData = require('../../utils/citydata.js');
-const times = {
-  '2018年': ['12月', '11月', '10月', '9月', '8月', '7月', '6月', '5月', '4月', '3月', '2月', '1月'],
-  '2017年': ['12月', '11月', '10月', '9月', '8月', '7月', '6月', '5月', '4月', '3月', '2月', '1月']
-};
+
 Page({
   data: {
   
     showDis: false,
     showTime :false,
-    columnsTime: [
-      {
-        values: Object.keys(times),
-        className: 'column1'
-      },
-      {
-        values: times['2018年'],
-        className: 'column2',
-        defaultIndex: 2
-      }
-    ],
+
     array:[1,2,3,4,5,6,7,8,9,10],
-    xinxinTime : '2019年1月',
-    selectTime:''
+
+    xinxiTime : '2019年1月',
+    xinxiArea:'广东-广州市',
+    
+    citysData: cityData.citysData,
+    provinces: [],
+    citys: [],
+    areas: [],
+    value: [0, 0, 0],
+    name: '',
+   
+    minDate: new Date(2016,1,1).getTime(),
+    maxDate: new Date(2019, 10, 1).getTime(),
+    currentDate: new Date().getTime()
+    
   },
 
   //弹出地区选择器
@@ -37,7 +37,7 @@ Page({
   timeClick: function () {
     this.setData({
       showTime: true,
-      selectTime: this.data.xinxinTime
+      
     })
   },
   //点击蒙层关闭地区选择器
@@ -52,28 +52,92 @@ Page({
       showTime: false
     })
   },
-  //时间选择器滑动
-  onChange(event) {
-    const { picker, value, index } = event.detail;
-    picker.setColumnValues(1, times[value[0]]);
+  
+  initData: function () {
+    var provinces = [];
+    var citys = [];
+    var areas = [];
+
+    this.data.citysData.forEach(function (province, i) {
+      provinces.push(province.name);
+      if (i === 0) {
+        citys.push(province.citys[i].name);
+        areas = province.citys[i].areas;
+      }
+    });
 
     this.setData({
-      selectTime: value[0] + value[1]
-    })
+      provinces: provinces,
+      citys: citys,
+      areas: areas
+    });
+  },
+
+  bindChange: function (e) {
+    var citysData = this.data.citysData;
+    var value = this.data.value;
+    var current_value = e.detail.value;
+    var citys = [];
+
+    var provinceObj = {};
+    var cityObj = {};
+
+    provinceObj = citysData[current_value[0]];
+
+    if (value[0] !== current_value[0]) {
+      // 滑动省份
+      provinceObj.citys.forEach(function (v) {
+        citys.push(v.name);
+      });
+      this.setData({
+        citys: citys
+      });
+
+      cityObj = provinceObj.citys[0];
+      this.setData({
+        areas: cityObj.areas,
+        value: [current_value[0], 0, 0]
+      });
+
+    } else if (value[0] === current_value[0] && value[1] !== current_value[1]) {
+      // 滑动城市
+      if (current_value[1] >= provinceObj.citys.length) {
+        // 数据不存在 跳过
+        return;
+      }
+      cityObj = provinceObj.citys[current_value[1]];
+      this.setData({
+        areas: cityObj.areas,
+        value: [current_value[0], current_value[1], 0]
+      });
+    } else {
+      // 滑动区县
+      cityObj = provinceObj.citys[current_value[1]];
+      this.setData({
+        value: current_value
+      });
+    }
+
+    this.setData({
+      name: provinceObj.name + '-' + cityObj.name + '-' + cityObj.areas[this.data.value[2]],
+      xinxiArea: provinceObj.name + '-' + cityObj.name
+    });
+    console.log('信息价地区修改为:'+this.data.xinxiArea)
+  },
+
+  // 页面初始化事件
+  onLoad: function () {
+    this.initData();
+  } ,
+  
+  
+  time_input(value) {
+    var time_str = new Date(value.detail).getFullYear() + '年' + (new Date(value.detail).getMonth() + 1) + '月'
+    this.setData({ xinxiTime: time_str})
     
   },
-  //时间选择器点击取消
-  onTimeCancel:function(){
-    this.setData({
-      showTime: false
-    })
-  },
-  onTimeConfirm:function(){
 
-    this.setData({
-      showTime: false,
-      xinxinTime:this.data.selectTime
-    })
-    console.log('信息价更新为'+this.data.xinxinTime)
-  }
+  
+  
+  
 });
