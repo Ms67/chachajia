fae23c696874be679f5b8d25d95ba5cca0ff664a
hxsscut <641700486@qq.com>
2019-04-01 17:18:06 +0800

    云函数 数据库添加

--- functions/add/index.js
+++ functions/add/index.js
@@ -1,16 +1,31 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
-
+const fs = require('fs')
+const path = require('path')
 cloud.init()
 
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext()
+  const fileID = 'cloud://xinxijia-dev-7f7d27.7869-xinxijia-dev-7f7d27/单个城市查询.json'
+  const res = await cloud.downloadFile({
+    fileID,
+  })
+  const buffer = res.fileContent
+  var datalist = JSON.parse(buffer.toString('utf8'))["RECORDS"]
+  const db = cloud.database()
+  for (i in datalist){
+    try {
+       await db.collection('test').add({
+        // data 字段表示需新增的 JSON 数据
+        data: datalist[i]
+      })
+      // console.log(datalist[i])
+      console.log("add")
+    } catch (e) {
+      console.error(e)
+    }
+  } 
+  
 
-  return {
-    event,
-    openid: wxContext.OPENID,
-    appid: wxContext.APPID,
-    unionid: wxContext.UNIONID,
-  }
+ 
 }
